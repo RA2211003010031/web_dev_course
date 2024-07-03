d54162c1377007c706744f5608b0301080b323bf
Adarsh <ar6893@srmist.edu.in>
2024-07-03 22:36:54 +0530

    video 55 uploaded

---
+++ 55. JavaScript Variables, Data Types & Objects/quiz/script.js
@@ -0,0 +1,7 @@
+let student = {
+    name : "Adarsh",
+    "phone number" : 939028098,
+    marks : 98
+}
+
+console.log(student)
---
+++ 55. JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,22 @@
+console.log("hey this si tut 55")
+
+var a = 5;
+var b = 6;
+var c = "adarsh"
+console.log(a+b + 10)
+
+console.log(typeof a, typeof c)
+
+
+// now only let is used as it is global variable and is used in latest javascript version.In any block, it is block variable too.
+
+let o = {
+    name : "Adarsh",
+    job : "No job",
+    "phone no" : 7893209818
+}
+
+console.log(o)
+
+o.name = "Adarsh Raj";
+console.log(o)
