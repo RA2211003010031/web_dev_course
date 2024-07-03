710a2b73dc9f147cb788ed71cfc88dc7dd0050be
Adarsh <ar6893@srmist.edu.in>
2024-07-03 10:10:24 +0530

    chapter 54 update

--- 54. introduction to javascript/script.js
+++ 54. introduction to javascript/script.js
@@ -1,1 +1,10 @@
 alert("Hey, this is the alert!")
+
+console.log("Code is running")
+
+var a = prompt("Enter your number; ")
+var isTrue = confirm("You liked it?")
+if(isTrue = true){
+    console.log("OKAY")
+}
+console.log("your number is", a)
