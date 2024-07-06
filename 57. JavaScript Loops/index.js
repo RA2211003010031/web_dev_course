a6b4dec458e83156fd2e32a1f04906e7996da7ca
Adarsh <ar6893@srmist.edu.in>
2024-07-07 00:02:01 +0530

    video 57 done

---
+++ 57. JavaScript Loops/index.js
@@ -0,0 +1,17 @@
+for (let index = 0; index < 11; index++) {
+    console.log(index)
+}
+
+let obj = {
+    name : "Adarsh",
+    reg : "RA2211003010031",
+    sec : "A1"
+}
+
+for (const key in obj) {
+    console.log(key)
+}
+
+for (const c of "obj") {
+    console.log(c)
+}
