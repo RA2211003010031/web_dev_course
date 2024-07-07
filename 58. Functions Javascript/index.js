551ced0902eb21eb7325f345daca4a9126fa3a7f
Adarsh <ar6893@srmist.edu.in>
2024-07-07 08:36:54 +0530

    adding notes and video 58

---
+++ 58. Functions Javascript/index.js
@@ -0,0 +1,32 @@
+// 1.
+let obj = {
+    harry:98,
+    rohan:70,
+    aakash:73
+}
+
+// let keys = Object.keys(obj)
+
+// for (let i = 0; i < keys.length; i++) {
+//     let key = keys[i]
+//     console.log(obj[key])
+// }
+
+
+
+
+// 2.
+// for (const marks in obj) {
+//     console.log(obj[marks])
+// }
+
+
+
+// 3.
+function print() {
+    console.log("Try again")
+}
+
+let num = 0
+
+const userInput = prompt("Please enter your name:");
