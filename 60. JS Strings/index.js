e0a624ca228e32a5487fecb6b429b0addc0134b7
Adarsh <ar6893@srmist.edu.in>
2024-07-09 20:03:08 +0530

    video 60 and 62 done

---
+++ 60. JS Strings/index.js
@@ -0,0 +1,6 @@
+console.log("This is a normal print statement")
+
+a = "Adarsh"
+b = "Gracey"
+
+console.log(`\` is used to write statements in this form and then \${var_name} ${a} and \${var_name} ${b}`)
---
+++ 63. Buisness name generator/index.js
@@ -0,0 +1,37 @@
+let first,second,third
+
+let rand = Math.random()
+if(rand<0.33){
+    first = "Crazy"
+}
+else if(rand<0.66 && rand>=0.33) {
+    first = "Amazing"
+}
+else{
+    first = "Fire"
+}
+
+rand = Math.random()
+if(rand<0.33){
+    second = "Engine"
+}
+else if(rand<0.66 && rand>=0.33) {
+    second = "Foods"
+}
+else{
+    second = "Garments"
+}
+
+rand = Math.random()
+if(rand<0.33){
+    third = "Bros"
+}
+else if(rand<0.66 && rand>=0.33) {
+    third = "Limited"
+}
+else{
+    third = "Hub"
+}
+
+
+console.log(`${first} ${second} ${third}`)
