807473b86a9db9d2497a1fab17861d5a08e5f500
Adarsh <ar6893@srmist.edu.in>
2024-07-10 09:09:46 +0530

    video 70 completed

---
+++ 70. exercise/script.js
@@ -0,0 +1,96 @@
+let color
+
+let rand = Math.random()
+if(rand<0.2) {
+    color = "red"
+}
+else if(rand>=0.2 && rand<0.4){
+    color = "green"
+}
+else if(rand>=0.4 && rand<0.6){
+    color = "blue"
+}
+else if(rand>=0.6 && rand<0.8){
+    color = "pink"
+}
+else{
+    color = "yellow"
+}
+
+document.body.childNodes[1].children[0].style.backgroundColor = color
+
+rand = Math.random()
+if(rand<0.2) {
+    color = "red"
+}
+else if(rand>=0.2 && rand<0.4){
+    color = "green"
+}
+else if(rand>=0.4 && rand<0.6){
+    color = "blue"
+}
+else if(rand>=0.6 && rand<0.8){
+    color = "pink"
+}
+else{
+    color = "yellow"
+}
+
+document.body.childNodes[1].children[1].style.backgroundColor = color
+
+rand = Math.random()
+if(rand<0.2) {
+    color = "red"
+}
+else if(rand>=0.2 && rand<0.4){
+    color = "green"
+}
+else if(rand>=0.4 && rand<0.6){
+    color = "blue"
+}
+else if(rand>=0.6 && rand<0.8){
+    color = "pink"
+}
+else{
+    color = "yellow"
+}
+
+document.body.childNodes[1].children[2].style.backgroundColor = color
+
+rand = Math.random()
+if(rand<0.2) {
+    color = "red"
+}
+else if(rand>=0.2 && rand<0.4){
+    color = "green"
+}
+else if(rand>=0.4 && rand<0.6){
+    color = "blue"
+}
+else if(rand>=0.6 && rand<0.8){
+    color = "pink"
+}
+else{
+    color = "yellow"
+}
+
+document.body.childNodes[1].children[3].style.backgroundColor = color
+
+rand = Math.random()
+if(rand<0.2) {
+    color = "red"
+}
+else if(rand>=0.2 && rand<0.4){
+    color = "green"
+}
+else if(rand>=0.4 && rand<0.6){
+    color = "blue"
+}
+else if(rand>=0.6 && rand<0.8){
+    color = "pink"
+}
+else{
+    color = "yellow"
+}
+
+document.body.childNodes[1].children[4].style.backgroundColor = color
